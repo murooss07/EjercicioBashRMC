f8c8c8453d52c1e79eb97fef73ef538cfe987520
murooss07 <rmuros144@ieszaidinvergeles.org>
2025-02-12 11:58:20 +0100

    Nuevo branch

--- open_google.sh
+++ open_google.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+# Definir nombre para la búsqueda en Google
 NOMBRE="SOY TU PEOR PESADILLA"
+
+# Abrir Google con el nombre
 xdg-open "https://www.google.com/search?q=${NOMBRE}"
+
+# Abrir la imagen de "hackeo"
 HACK_IMAGE="https://i.imgur.com/EsnPdi3.jpg"
 sleep 2
 xdg-open "${HACK_IMAGE}"
-#chmod +x open_google.sh
-echo "$(pwd)/open_google.sh" >> ~/.bashrc
+
